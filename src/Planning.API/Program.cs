4a2f48918eeef79f3628a2e23aab9b8d699ea791
K.Solomatin <k.solomatin@agrohold.ru>
2025-04-08 16:28:32 +0300

    Создание новой ветки

--- src/Planning.API/Program.cs
+++ src/Planning.API/Program.cs
@@ -34,6 +34,10 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
+app.MapGet("item-stock", () =>
+{
+
+}).WithName("GetItemStock");
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
