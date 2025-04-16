07fd6034a2b8ec492ab01d0b268aaf9470db9be4
K.Solomatin <k.solomatin@agrohold.ru>
2025-04-16 11:50:13 +0300

    Реализовал api Planning. Добавил на страницу клиента "Planning" информацию по артикулам.

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddScoped(sp =>
+    new HttpClient { BaseAddress = new Uri("http://localhost:5093") });
 
 var app = builder.Build();
 
@@ -15,7 +17,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 
 
